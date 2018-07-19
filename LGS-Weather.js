b38f6b6bfa45178b942027d8bbb7949122d436f8
Hennadii Levinson <a.levinson.b@gmail.com>
2018-07-20 01:50:53 +0200

    changed  size of Module icons and texts

--- LGS-Weather.js
+++ LGS-Weather.js
@@ -81,14 +81,15 @@ Module.register("LGS-Weather", {
         return [
 		"LGS-Weather.css", 
 		"weather-icons.css",
-		//"https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css"
+		"bootstrap.min.prefixed.css"		
 		];
     },
     getScripts: function() {
         return [
 		"moment.js", 
 		"fontawesome-all.js",
-		//"https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js"
+		"jquery-3.3.1.js",
+		"bootstrap.min.prefixed.js"
 		];
     },
 
@@ -108,7 +109,7 @@ Module.register("LGS-Weather", {
 	l = 1;
 	loco = this.config.loco1;
 	this.config.pws = this.config.pws1; 
-        this.forecast = {};
+        this.forecast = [];
 	    this.forecast2 = {};
         this.air = {};
         this.srss = {};
@@ -118,7 +119,7 @@ Module.register("LGS-Weather", {
         this.city = {};
         this.clphase = {};
         this.current = {};
-        this.allDay = {};
+        this.allDay = {};//all night
         this.today = "";
         
     }, 
@@ -189,99 +190,104 @@ Module.register("LGS-Weather", {
     },
 
 
-    
-
-    getDomInside: function(){
-        	var reading = this.lastReading;
-		
-
-        var weatherTable = document.createElement("table");
-        weatherTable.classList.add("table");
-		
-		var temperatureIcon = document.createElement("i");
-		var humidityIcon = document.createElement("i");
-		
-		temperatureIcon.className = "fas fa-thermometer-half";
-		humidityIcon.className = "fas fa-tint";
-		
-		temperatureIcon.setAttribute("color", "#e2fdec");
-		humidityIcon.setAttribute("color", "#e2fdec");
-		
-
-        var forecastRow = document.createElement("tr");
-
-        var second = document.createElement("th");
-        second.setAttribute("colspan", 2);
-	second.appendChild(temperatureIcon);
-        forecastRow.appendChild(second);
-
-        var third = document.createElement("th");
-	third.setAttribute("colspan", 2);
-        third.appendChild(humidityIcon);
-        forecastRow.appendChild(third);
-      
-        var TDrow = document.createElement("tr");
-        TDrow.classList.add("xsmall", "bright");
-
-        var td2 = document.createElement("td");
- 	td2.setAttribute("colspan", 2);     
-        TDrow.appendChild(td2);
-
-        var td3 = document.createElement("td");
-	td3.setAttribute("colspan", 2);       
-        TDrow.appendChild(td3);
-        
-
-        if (reading){
-			td2.innerHTML = reading.temperature+"&#8451;";
-			td3.innerHTML = reading.humidity+"%";
-		}
-		else{
-			wrapper.innerHTML = "Initializing...";
-	}
-
-	weatherTable.appendChild(forecastRow);
-	weatherTable.appendChild(TDrow);
-	var wrapper = document.createElement("tr");
-	var cell = document.createElement("td");
-	cell.setAttribute("colspan", 4);
-	cell.appendChild(weatherTable);
-	wrapper.appendChild(cell);
-	return wrapper;
-    },
+ 
 
     getSuSd: function() {
-	var weatherTable = document.createElement("table");
-        weatherTable.classList.add("table");
+	
+	
+	
+	return [iconsRow, valuesRow];
+    },
 
-	var SUSymbol = document.createElement("img");
-	var SDSymbol = document.createElement("img");
-     
-	SUSymbol.id = "x";
-	SUSymbol.src = "modules/LGS-Weather/images/icons/Sunrise.png";
-	SUSymbol.width = 30;
-	SUSymbol.height = 30;
+    getDom: function() {
+	
+	function create(el){ return document.createElement(el)}
+	function wrapper(children){
+		var el = create("div");
+		el.classList.add("tb-container");
+		children.forEach(c => el.appendChild(c));
+		return el;
+	}
+	function row(children){
+		
+		var el = create("div");
+		el.classList.add("tb-row");
+		children.forEach(c => c.classList.add("tb-text-center", "tb-col-xs-"+parseInt(12/children.length)));
+		children.forEach(c => el.appendChild(c));
+		return el;
+	}
+	function col(arg){
+		
+		var el = create("div");
+		
+		if (arg instanceof Array){
+			
+			arg.forEach(c => el.appendChild(c));
+		}
+		else{
+			
+			el.innerHTML = arg;
+		}
+		return el;
+	}
+	function img(src, cls){
+		var el = create("img");
+		el.src = src;
+		el.classList.add(cls);
+		return el;
+	}
 
-	SDSymbol.id = "y";
-	SDSymbol.src = "modules/LGS-Weather/images/icons/Sunset.png";
-	SDSymbol.width = 30;
-	SDSymbol.height = 30;
+	function classAdder(cls){
+		return function(el){
+			el.classList.add(cls);
+			return el;
+		}
+	}
 
-	var midRow = document.createElement("tr");
- 
-        var sunup = document.createElement("th");
-	sunup.setAttribute("colspan", 2);
-	sunup.appendChild(SUSymbol);
-        midRow.appendChild(sunup);
+	var addSmallTextClass = classAdder("text-weather-info");
+
+	var smIconCls = "icon3";
+        var d = new Date();
+        var n = d.getHours();
+	var reading = this.lastReading;
+	var current = this.current;
+	
+	var dayImageSrc = 'modules/LGS-Weather/images/' + current.icon + '.png';
+	var nightImageSrc = 'modules/LGS-Weather/images/nt_' + current.icon + '.png';
+	var weatherImageSrc = (n < 22 && n > 5) ? dayImageSrc : nightImageSrc;
+	
+	var weatherImage = img(weatherImageSrc, "icon2");
+	var topTemperature = document.createElement("div");
+	var weatherText = document.createElement("div");
+	
+	weatherText.innerHTML = current.weather;
+	weatherText.classList.add("medium", "weather-header", "bright");
+	
+	topTemperature.classList.add("weather-temperature", "large", "tempf");
+	
+	if (reading){
+		topTemperature.innerHTML = Math.round(reading.temperature) + "&deg;";			
+	}	
+
+	
+	var imgAndTemp = document.createElement("div");
+	imgAndTemp.appendChild(weatherImage);
+	imgAndTemp.appendChild(topTemperature);
+	
+	
+	var headerCol = col([weatherText, imgAndTemp]);
+	var headerRow = row([headerCol]);
+	
+        var SUSymbol = img("modules/LGS-Weather/images/icons/Sunrise.png", smIconCls);
+	var SDSymbol = img("modules/LGS-Weather/images/icons/Sunset.png", smIconCls)
+    
+	
+	var srssIconsRow = row([
+		col([SUSymbol]),
+		col([SDSymbol])
+	]);
 
 
-	var sunDown = document.createElement("th");
-	sunDown.setAttribute("colspan", 2);
-	sunDown.appendChild(SDSymbol);
-        midRow.appendChild(sunDown);
-
-
-        weatherTable.appendChild(midRow);
 
         var srss = this.srss;
         var sunrise = srss.sunrise;
@@ -291,154 +297,31 @@ Module.register("LGS-Weather", {
 	var sunrise = config.timeFormat == 12 ? moment(utcsunrise).local().format("h:mm A") : moment(utcsunrise).local().format("HH:mm");
 	var sunset = config.timeFormat == 12 ? moment(utcsunset).local().format("h:mm A") : moment(utcsunset).local().format("HH:mm");
 
-	var convertedTimeSr = parseInt(sunrise);
-	var convertedTimeSs = parseInt(sunset);
-
-        var Midrow = document.createElement("tr");
-        Midrow.classList.add("xsmall", "bright");
-        Midrow.setAttribute('style', 'line-height: 30%;');
-
-        var SU = document.createElement("td");
-	SU.setAttribute("colspan", 2);
-        SU.innerHTML = sunrise;
-        Midrow.appendChild(SU);
-
-
-        var SD = document.createElement("td");
-	SD.setAttribute("colspan", 2);
-        SD.innerHTML = sunset;
-        Midrow.appendChild(SD);
-
-        weatherTable.appendChild(Midrow);
-
-
-	var wrapper = document.createElement("tr");
-	var cell = document.createElement("td");
-	cell.setAttribute("colspan", 4);
-	cell.appendChild(weatherTable);
-	wrapper.appendChild(cell);
-	return wrapper;
-    },
-
-    getDom: function() {
-
-
-        var wrapper = document.createElement("div");
-
+	var srssValuesRow = row([
+		addSmallTextClass(col(sunrise)),
+		addSmallTextClass(col(sunset))
+	]);
 	
 
-        var current = this.current;
-
-        var d = new Date();
-        var n = d.getHours();
-
-        //var curCon = document.createElement("div");
-    	
-        //wrapper.appendChild(curCon);
-
-	var reading = this.lastReading;
-
-
-        var cur = document.createElement("div");
-
-	cur.classList.add("weather-table");
+        var currentHSymbol = img("modules/LGS-Weather/images/icons/Feuchtigkeit.png", smIconCls);
+	var tempSymbol = img("modules/LGS-Weather/images/icons/Regen.png", smIconCls);
 	
-	var weatherImage = document.createElement("img");
-	var topTemperature = document.createElement("div");
-	var weatherText = document.createElement("div");
-	
-	weatherText.innerHTML = current.weather;
-
-	weatherText.classList.add("medium");
-	cur.classList.add("weather-header", "bright");
-	
-	topTemperature.classList.add("weather-temperature", "large", "tempf");
-	
-	if (reading){
-		topTemperature.innerHTML = Math.round(reading.temperature) + "&deg;";			
-	}	
+        var currentWindSymbol = img("modules/LGS-Weather/images/icons/Windgeschiwndigkeit.png", smIconCls);
 
 
-	weatherImage.classList.add("icon2", "small", "bright", "img");
-	var dayImageSrc = 'modules/LGS-Weather/images/' + current.icon + '.png';
-	var nightImageSrc = 'modules/LGS-Weather/images/nt_' + current.icon + '.png';
-	weatherImage.src = (n < 22 && n > 6) ? dayImageSrc : nightImageSrc;
+	var currentForecastIconsRow = row([
+		col([tempSymbol]), 
+		col([currentHSymbol]), 
+		col([currentWindSymbol])
+		]); 
 
-	cur.appendChild(weatherText);
-	cur.appendChild(weatherImage);
-	cur.appendChild(topTemperature);
-        cur.setAttribute('style', 'line-height: 105%;');
-   	wrapper.appendChild(cur);
-
-        var top = document.createElement("div");
-
-	var weatherTable = document.createElement("table");
-        weatherTable.classList.add("table");
-	weatherTable.classList.add("weather-table");
-
-        weatherTable.appendChild(this.getSuSd());
-
-        top.appendChild(weatherTable);
-        wrapper.appendChild(top);
-	
-
-        var forecastRow = document.createElement("tr");
-
-        var second = document.createElement("th");
-        var tempSymbol = document.createElement("img");
-		
-	tempSymbol.src = "modules/LGS-Weather/images/icons/Regen.png";
-	tempSymbol.width = 30;
-	tempSymbol.height = 30;
-        second.appendChild(tempSymbol);
-        forecastRow.appendChild(second);
-
-        var third = document.createElement("th");
-        var currentHSymbol = document.createElement("img");
-
-	currentHSymbol.src = "modules/LGS-Weather/images/icons/Feuchtigkeit.png";
-	currentHSymbol.width = 30;
-	currentHSymbol.height = 30;
-        third.appendChild(currentHSymbol);
-        forecastRow.appendChild(third);
-
-        var fourth = document.createElement("th");
-        var currentWindSymbol = document.createElement("img");
-	
-        currentWindSymbol.src = "modules/LGS-Weather/images/icons/Windgeschiwndigkeit.png";
-	currentWindSymbol.width = 30;
-	currentWindSymbol.height = 30;
-        fourth.appendChild(currentWindSymbol);
-        forecastRow.appendChild(fourth);
-
-        weatherTable.appendChild(forecastRow);
-
-        var TDrow = document.createElement("tr");
-        TDrow.classList.add("xsmall", "bright");
-
-        var td2 = document.createElement("td");
-        td2.innerHTML = current.rain_probability;
-        TDrow.appendChild(td2);
-        weatherTable.appendChild(TDrow);
-
-        var td3 = document.createElement("td");
-	if (reading) {
-		td3.innerHTML = reading.humidity + "%";
-	}
-        TDrow.appendChild(td3);
-        weatherTable.appendChild(TDrow);
-
-        var td5 = document.createElement("td");
-        td5.innerHTML = current.wind_mph;
-        TDrow.appendChild(td5);
-        weatherTable.appendChild(TDrow);
+	var currentForcastValuesRow = row([
+		addSmallTextClass(col(current.rain_probability)),
+		addSmallTextClass(col(reading ? parseInt(reading.humidity) + "%" : "")),
+		addSmallTextClass(col(current.wind_mph))
+	]);        
 
         ////// FORECAST ROWS ///////////////////////////////// 
-
-        var ForecastTable = document.createElement("table");
-        ForecastTable.classList.add("table")
-        ForecastTable.setAttribute('style', 'line-height: 20%;');
-	ForecastTable.setAttribute("style", "padding-top: 30px;");
 
         var d = new Date();
         var weekday = new Array(7);
@@ -451,46 +334,49 @@ Module.register("LGS-Weather", {
         weekday[6] = "Sat";
 
         var n = this.translate(weekday[d.getDay()]);
+	var self = this;
+	var weekForcastDaysRow = row(
+		self.forecast.map(
+			(noaa) =>
+			(self.translate(noaa.date.weekday_short) == n) ? 
+				self.translate("Today") : 
+				self.translate(noaa.date.weekday_short)
+            
+		).map(col).map(classAdder("text-weather-info"))
+	);
 
-
-        var nextRow = document.createElement("tr");
-        for (i = 0; i < this.forecast.length; i++) {
-            var noaa = this.forecast[i];
-            var wdshort = document.createElement("td");
-            wdshort.classList.add("xsmall", "bright");
-            wdshort.setAttribute("style", "padding:10px");
-    	    wdshort.innerHTML = (this.translate(noaa.date.weekday_short) == n) ? this.translate("Today") : this.translate(noaa.date.weekday_short);
-            nextRow.appendChild(wdshort);
-            ForecastTable.appendChild(nextRow);
-        }
-
-        var foreRow = document.createElement("tr");
-        for (i = 0; i < this.forecast.length; i++) {
-            var noaa = this.forecast[i];
-            var fore = document.createElement("td");
-
-            fore.setAttribute("colspan", "1");
-            fore.innerHTML = "<img src='modules/LGS-Weather/images/" + noaa.icon + ".png' height='22' width='28'>";
-            foreRow.appendChild(fore);
-            ForecastTable.appendChild(foreRow);
-        }
-
-        var tempRow = document.createElement("tr");
-        for (i = 0; i < this.forecast.length; i++) {
-            var noaa = this.forecast[i];
-            var temper = document.createElement("td");
-            temper.setAttribute("colspan", "1");
-            temper.classList.add("xsmall", "bright");
-            temper.innerHTML = (config.units != "metric") ? noaa.high.fahrenheit + "/" + noaa.low.fahrenheit : noaa.high.celsius + "/" + noaa.low.celsius;
-            tempRow.appendChild(temper);
-            ForecastTable.appendChild(tempRow);
-
-        }
-
-        wrapper.appendChild(ForecastTable);
-
+	var weekForcastIconsRow = row(
+		self.forecast.map(
+			(noaa) =>
+			"<img src='modules/LGS-Weather/images/" + 
+			noaa.icon + ".png' class='"+smIconCls+"'>"            
+		).map(col).map(classAdder("text-weather-info"))
+	);
         
-        return wrapper;
+	var weekForcastValuesRow = row(
+		self.forecast.map(
+			(noaa) =>
+			(config.units != "metric") ? 
+			noaa.high.fahrenheit + "/" + noaa.low.fahrenheit : 
+			noaa.high.celsius + "/" + noaa.low.celsius
+                   
+		).map(col).map(classAdder("text-weather-info"))
+	);
+        
+
+ 	
+        
+        return wrapper([
+		headerRow,
+		srssIconsRow,
+		srssValuesRow,
+		currentForecastIconsRow,
+		currentForcastValuesRow,
+		weekForcastDaysRow,
+		weekForcastIconsRow,
+		weekForcastValuesRow
+			
+	]);
 
     }, 
 });
